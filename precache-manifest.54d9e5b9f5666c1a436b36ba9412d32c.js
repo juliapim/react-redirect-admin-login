5818baae9ec85d2d890dc585d732a060b0fbf53f
DCI-PC20 <juliarpim@gmail.com>
2020-06-04 13:42:55 +0200

    Updates

---
+++ precache-manifest.54d9e5b9f5666c1a436b36ba9412d32c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c6051ac04ac213db2976ebb5a2b0fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f6cf3a4c349f7a7b6220",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9dc13b0e497f15d1a437",
+    "url": "./static/js/2.3637592e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3637592e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6cf3a4c349f7a7b6220",
+    "url": "./static/js/main.f036582b.chunk.js"
+  },
+  {
+    "revision": "78075bafd6f6c3001f91",
+    "url": "./static/js/runtime-main.8c7bbbdd.js"
+  }
+]);
